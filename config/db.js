13b92ff4efea949a6b799c3862138201e680a78b
Aditya Zaveri <adityazaveri7@gmail.com>
2024-11-07 17:18:25 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',        // PostgreSQL username
-    host: 'localhost',            // Hostname
-    database: 'task_manager',     // Database name
-    password: 'tiger',    // PostgreSQL password
-    port: 5432,                   // Default PostgreSQL port
+    user: 'postgres',        
+    host: 'localhost',           
+    database: 'task_manager',    
+    password: 'tiger',    
+    port: 5432,                  
 });
 
 module.exports = pool;
